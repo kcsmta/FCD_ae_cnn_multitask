bb13b0098434fc2d3072ea519506eb343070f5a2
kcsmta <tungkhanhmta@gmail.com>
2020-03-25 01:10:36 +0700

    update code

--- FCD_models/run_train_cnn_multi_task.py
+++ FCD_models/run_train_cnn_multi_task.py
@@ -1,5 +1,5 @@
 import sys
-import ae_cnn_asm_multi_task_NoPretrain_train
+import cnn_asm_multi_task_train
 
 
 if __name__ =='__main__':
@@ -8,4 +8,4 @@ if __name__ =='__main__':
     probs = ["FLOW016", "MNMX", "SUBINC", "SUMTRIAN"]
     for p in probs:
         print ("run training on ", p)
-        ae_cnn_asm_multi_task_NoPretrain_train.run_net(probs=[p])
+        cnn_asm_multi_task_train.run_net(probs=[p])
